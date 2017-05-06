04b3809e1602b7b18e9e8e99030f2d99cb0af207
Jay <shhahjay@gmail.com>
2017-05-06 18:38:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jay/battery_level_broadcast_receiver/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.jay.battery_level_broadcast_receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView btv;
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context arg0, Intent intent) {
+            // TODO Auto-generated method stub
+            int level = intent.getIntExtra("level", 0);
+            btv.setText("Battery Level Remaining:"+String.valueOf(level) + "%");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btv=(TextView) findViewById(R.id.tv1);
+        this.registerReceiver(this.mBatInfoReceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+    }
+}
